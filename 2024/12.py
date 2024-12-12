4687696b25cbc920adf39468bbe53f13572ea141
Micke <mikael.molsa@proton.me>
2024-12-12 10:08:56 +0200

    Minor cleanup.

--- 2024/12.py
+++ 2024/12.py
@@ -15,13 +15,7 @@ ymax = len(data) - 1
 xmax = len(data[0]) - 1
 unvisited = {(y, x) for y in range(ymax + 1) for x in range(xmax + 1)}
 dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
-convex_edges = (
-    ((0, -1), (-1, 0)),  # top left
-    ((0, 1), (-1, 0)),  # top right
-    ((0, -1), (1, 0)),  # bottom left
-    ((0, 1), (1, 0)),  # bottom right
-)
-concave_edges = (
+edges = (
     ((0, -1), (-1, 0), (-1, -1)),  # top left
     ((0, 1), (-1, 0), (-1, 1)),  # top right
     ((0, -1), (1, 0), (1, -1)),  # bottom left
@@ -50,11 +44,11 @@ while unvisited:
                 perimeter -= 1
                 if (y + dy, x + dx) in unvisited and (y + dy, x + dx) not in q:
                     q.append((y + dy, x + dx))
-        for (dy1, dx1), (dy2, dx2) in convex_edges:
+        for (dy1, dx1), (dy2, dx2), _ in edges:  # Convex shape
             if (get(y + dy1, x + dx1) != c
                     and get(y + dy2, x + dx2) != c):
                 corners += 1
-        for (dy1, dx1), (dy2, dx2), (dy3, dx3) in concave_edges:
+        for (dy1, dx1), (dy2, dx2), (dy3, dx3) in edges:  # Concave shape
             if (get(y + dy1, x + dx1) == c
                     and get(y + dy2, x + dx2) == c
                     and get(y + dy3, x + dx3) != c):
