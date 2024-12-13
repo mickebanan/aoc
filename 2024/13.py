fbbc3c960d0cce1409fe28df5f887382defabd07
Micke <mikael.molsa@proton.me>
2024-12-13 10:12:02 +0200

    Fix up the test data.

--- 2024/13.py
+++ 2024/13.py
@@ -5,19 +5,19 @@ import numpy as np
 data = """
 Button A: X+94, Y+34
 Button B: X+22, Y+67
-Prize: X=10000000008400, Y=10000000005400
+Prize: X=8400, Y=5400
 
 Button A: X+26, Y+66
 Button B: X+67, Y+21
-Prize: X=10000000012748, Y=10000000012176
+Prize: X=12748, Y=12176
 
 Button A: X+17, Y+86
 Button B: X+84, Y+37
-Prize: X=10000000007870, Y=10000000006450
+Prize: X=7870, Y=6450
 
 Button A: X+69, Y+23
 Button B: X+27, Y+71
-Prize: X=10000000018641, Y=10000000010279
+Prize: X=18641, Y=10279
 """.split('\n\n')
 data = open('data/13.dat').read().split('\n\n')
 machines = []
